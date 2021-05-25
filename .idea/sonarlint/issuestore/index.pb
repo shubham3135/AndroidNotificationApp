8a156b1113d0a5d84231b7a05ed88a41704a364b
Shubham <shubhamkumarwinner@gmail.com>
2021-05-25 17:42:38 +0530

    Creating Custom layout for notification

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -28,5 +28,3 @@ T
 $.idea/codeStyles/codeStyleConfig.xml,d\d\dd7b3350c29c49497756c7f32c69a3bed18b1cba
 L
 .idea/codeStyles/Project.xml,6\e\6ecd6000a7b6f4a2884412ff19f74193ed089648
-P
- app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
--- app/src/main/java/com/shubhamkumarwinner/notificationtut/util/NotificationUtils.kt
+++ app/src/main/java/com/shubhamkumarwinner/notificationtut/util/NotificationUtils.kt
@@ -5,6 +5,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import com.shubhamkumarwinner.notificationtut.DomainActivity
 import com.shubhamkumarwinner.notificationtut.R
@@ -13,6 +14,7 @@ import com.shubhamkumarwinner.notificationtut.R
 const val CHANNEL_ID = "myChannel"
 private const val NOTIFICATION_ID = 0
 val GROUP_KEY_WORK_EMAIL = "com.shubhamkumarwinner.NotificationApp.WORK_EMAIL"
+val packageName = "com.shubhamkumarwinner.notificationtut"
 
 fun NotificationManager.sendNotification(title: String, messageBody: String, applicationContext: Context){
     //for tap option
@@ -32,27 +34,18 @@ fun NotificationManager.sendNotification(title: String, messageBody: String, app
         applicationContext, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT
     )
 
+    val notificationLayout = RemoteViews(packageName, R.layout.notification_small)
+    val notificationLayoutExpanded = RemoteViews(packageName, R.layout.notification_large)
+
 
     val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
         .setSmallIcon(R.drawable.ic_electric_bike)
-        .setContentTitle(title)
-        .setContentText(messageBody)
         .setPriority(NotificationCompat.PRIORITY_HIGH)
-        .setLargeIcon(myBitmap)
-        .setGroup(GROUP_KEY_WORK_EMAIL)
-        .setContentIntent(notifyPendingIntent)
-        // show custom number in notification badges
-        .setNumber(20)
-        //setBadgeIcon type
-        .setBadgeIconType(NotificationCompat.BADGE_ICON_NONE)
+        .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+        .setCustomContentView(notificationLayout)
+        .setCustomBigContentView(notificationLayoutExpanded)
 
-
-    // create different notification id for group notifications
     notify(NOTIFICATION_ID, builder.build())
-    notify(1, builder.build())
-    notify(2, builder.build())
-    notify(3, builder.build())
-    notify(4, builder.build())
 
 }
 
